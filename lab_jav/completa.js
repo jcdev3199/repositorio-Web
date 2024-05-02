435c226164b751eeac9bd6d68368fe556d0c2acb
jcdev3199 <168743953+jcdev3199@users.noreply.github.com>
2024-05-02 09:50:46 -0400

    Add files via upload

---
+++ lab_jav/completa.js
@@ -0,0 +1,21 @@
+const images = document.querySelector('.image');
+const modal = document.getElementById('modal');
+const modalDescription= document.getElementById('modal-description');
+
+images.forEach(image => {
+    image.addEventListener('click', () => {
+        modalDescription.textContent= image.dataset.description;
+        modal.style.display = 'block';
+    });
+});
+
+
+document.querySelector('.close').addEventListener('click', () =>{
+    modal.style.display = 'none';
+});
+
+window.addEventListener('click', (event )=>{
+    if(event.target === modal){
+        modal.style.display= 'none';
+    }
+});
